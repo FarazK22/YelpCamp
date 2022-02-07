293f405304d844576df8529b17813ad4d7fa745f
Faraz Kazi <faraz.kazi22@gmail.com>
2022-02-06 18:11:34 -0800

    Review model added with very basic styling

--- schemas.js
+++ schemas.js
@@ -12,3 +12,9 @@ module.exports.campgroundSchema = Joi.object({
 
 })
 
+module.exports.reviewSchema = Joi.object({
+    review: Joi.object({
+        rating: Joi.number().required(),
+        body: Joi.string().required()
+    }).required()
+})
